85519f57361e89ff3e4d89cd731bb3df103ab41c
Galaxy <jojomalrbx@gmail.com>
2020-05-22 17:38:40 -0500

    Update settings.lua

--- Galaxy's Bot/modules/settings.lua
+++ Galaxy's Bot/modules/settings.lua
@@ -1,8 +1,8 @@
 
 local settings
 settings = {
-  prefix = "$",
-  bot_version = "v0.1A",
-  debug_mode = false,
+  prefix = "$", -- change this to change the bot's prefix.
+  bot_version = "v0.1A", -- this doesn't do anything unless you want it to.
+  debug_mode = false, -- makes it so only the bot owner can use commands.
 }
 return settings
